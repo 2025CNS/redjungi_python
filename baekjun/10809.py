7c9bc5a87c47aee59d1a1cc326d52edff52bee0f
redjungi <150891110+redjungi09@users.noreply.github.com>
2025-03-21 09:53:26 +0900

    10809번

--- baekjun/10809.py
+++ baekjun/10809.py
@@ -1,7 +1,12 @@
 s = input()
-alp = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+alp = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+check = 0
 for i in range(26):
+  check = 0
   for k in range(len(s)):
     if alp[i] == s[k]:
       print(k,end=' ')
-  
+      check += 1
+      break
+  if check == 0:
+    print(-1,end=" ")
