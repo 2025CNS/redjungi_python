c9365c902ef84995c389272dcb7232d59db0620c
redjungi <150891110+redjungi09@users.noreply.github.com>
2025-03-24 20:19:40 +0900

    2231번

--- baekjun/2231.py
+++ baekjun/2231.py
@@ -1,1 +1,13 @@
-?
+?n = int(input())
+l = n-len(str(n))*9
+if l < 0:
+  l = 0
+result = 0
+for i in range(l, n+1):
+    nums = list(map(int, str(i)))
+    result = sum(nums) + i
+    if result == n:
+        print(i)
+        break
+    if i == n:
+        print(0)
