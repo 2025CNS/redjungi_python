ee0f846e7d85ec00fd4ab19a1891dc76a843fee0
redjungi <150891110+redjungi09@users.noreply.github.com>
2025-03-31 19:29:18 +0900

    2839번
    실5

---
+++ baekjun/7568.py
@@ -0,0 +1,19 @@
+import sys
+t = int(input())
+n=[]
+count = 0
+rank = []
+for _ in range(t):
+  n.append(list(map(int,sys.stdin.readline().split())))
+
+# for i in range(t):
+#   for k in range(1):
+#     print(n[i][k],n[i][k+1])
+for i in range(t):
+    cnt = 0
+    for j in range(t):
+        if n[i][0] < n[j][0] and n[i][1] < n[j][1]:
+            cnt += 1
+    rank.append(cnt+1)
+
+print(*rank)
