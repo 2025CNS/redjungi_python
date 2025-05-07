3a3229b2b3b9e940152401b05b198203d98cfb14
redjungi <150891110+redjungi09@users.noreply.github.com>
2025-05-07 11:14:32 +0900

    9095번
    실3

--- baekjun/9095.py
+++ baekjun/9095.py
@@ -1,4 +1,12 @@
 t = int(input())
-def dp(n):
-  dp = [0]*n
-  for i in range()
+dp = [0]*12
+dp[1] = 1
+dp[2] = 2
+dp[3] = 4
+for i in range(4,12):
+  dp[i] = dp[i-3]+dp[i-2]+dp[i-1]
+result = []
+for i in range(t):
+  n = int(input())
+  result.append(dp[n])
+print("\n".join(map(str, result)))
